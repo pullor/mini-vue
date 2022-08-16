193168f16e569dcb0c71126f68644cb370518a87
keke <asd_email@163.com>
2022-08-16 16:51:58 +0800

    reactive,effect依赖收集和触发依赖

---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,17 @@
+import { track, trigger } from "./effect"
+
+
+export const reactive = function(data) {
+    return new Proxy(data, {
+        get(target, key) {
+            const res = Reflect.get(target, key)
+            track(target, key)
+            return res
+        },
+        set(target, key, value) {
+            const res = Reflect.set(target, key, value)
+            trigger(target, key)
+            return res
+        }
+    })
+}
