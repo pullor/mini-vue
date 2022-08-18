79d14efb4f7ae44252ad80bb62589b6482d0997b
keke <asd_email@163.com>
2022-08-18 16:56:07 +0800

    shapeFlages

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,5 @@
+import { ShapeFlags } from '../shared/ShapeFlags'
+
 import { isObject } from '../shared/index'
 import { createComponentInstance, setupComponent } from './component'
 
@@ -6,9 +8,10 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  if (typeof vnode.type === 'string') {
+  const { shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container)
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container)
   }
 }
@@ -20,12 +23,12 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type))
 
-  const { children } = vnode
+  const { children, shapeFlag } = vnode
 
   // children
-  if (typeof children === 'string') {
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el)
   }
 
