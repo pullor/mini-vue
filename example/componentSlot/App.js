86db2fa92c6eb43bd100bb44bd238b6d071431a9
keke <asd_email@163.com>
2022-08-19 14:26:38 +0800

    Fragment和Text 节点类型

--- example/componentSlot/App.js
+++ example/componentSlot/App.js
@@ -1,25 +1,25 @@
-import { h } from "../../lib/guide-mini-vue.esm.js";
-import { Foo } from "./Foo.js";
+import { h, createTextVNode } from '../../lib/guide-mini-vue.esm.js'
+import { Foo } from './Foo.js'
 
 export const App = {
-  name: "App",
+  name: 'App',
   render() {
-    const app = h("div", {}, "App");
+    const app = h('div', {}, 'App')
     // object key
     const foo = h(
       Foo,
       {},
       {
-        header: ({ age }) => h("p", {}, "header" + age),
-        footer: () => h("p", {}, "footer"),
+        header: ({ age }) => [h('p', {}, 'header' + age), createTextVNode('你好呀')],
+        footer: () => h('p', {}, 'footer'),
       }
-    );
+    )
     // 数组 vnode
     // const foo = h(Foo, {}, h("p", {}, "123"));
-    return h("div", {}, [app, foo]);
+    return h('div', {}, [app, foo])
   },
 
   setup() {
-    return {};
+    return {}
   },
-};
+}
