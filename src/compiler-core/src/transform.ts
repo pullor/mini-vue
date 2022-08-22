33bc2ecac14e0b68479ae411c8f8bcc3fdd7e74e
keke <asd_email@163.com>
2022-08-22 09:51:18 +0800

    代码生成string

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,33 +1,38 @@
-export function transform(root, options) {
-  const context = createTransformContext(root, options);
-  traverseNode(root, context);
+export function transform(root, options = {}) {
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
+  createRootCodegen(root)
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0]
 }
 
 function createTransformContext(root: any, options: any): any {
   const context = {
     root,
     nodeTransforms: options.nodeTransforms || [],
-  };
+  }
 
-  return context;
+  return context
 }
 
 function traverseNode(node: any, context) {
-  const nodeTransforms = context.nodeTransforms;
+  const nodeTransforms = context.nodeTransforms
   for (let i = 0; i < nodeTransforms.length; i++) {
-    const transform = nodeTransforms[i];
-    transform(node);
+    const transform = nodeTransforms[i]
+    transform(node)
   }
 
-  traverseChildren(node, context);
+  traverseChildren(node, context)
 }
 function traverseChildren(node: any, context: any) {
-  const children = node.children;
+  const children = node.children
 
   if (children) {
     for (let i = 0; i < children.length; i++) {
-      const node = children[i];
-      traverseNode(node, context);
+      const node = children[i]
+      traverseNode(node, context)
     }
   }
 }
