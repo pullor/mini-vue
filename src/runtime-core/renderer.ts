19fa6ed6a15f52feffab799552d2b52dde9ab7a3
keke <asd_email@163.com>
2022-08-22 10:19:57 +0800

    编译template成 render函数

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -306,7 +306,7 @@ export function createRenderer(options) {
         if (!instance.isMounted) {
           console.log('init')
           const { proxy } = instance
-          const subTree = (instance.subTree = instance.render.call(proxy))
+          const subTree = (instance.subTree = instance.render.call(proxy, proxy))
 
           patch(null, subTree, container, instance, anchor)
 
@@ -323,7 +323,7 @@ export function createRenderer(options) {
           }
 
           const { proxy } = instance
-          const subTree = instance.render.call(proxy)
+          const subTree = instance.render.call(proxy, proxy)
           const prevSubTree = instance.subTree
           instance.subTree = subTree
 
